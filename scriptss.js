2dcedcf9dfd2fc3dec38a63e6c0a66cea032609d
wura-ola <wuraademola@gmail.com>
2023-06-30 20:58:33 +0100

    second commit

--- scriptss.js
+++ scriptss.js
@@ -16,17 +16,42 @@
 // const pins = [];
 
 function atmValidation(arr, pins) {
-  if (Array.isArray(arr)) {
-    arr.map((num) => {
-      console.log(num);
-      if (pins === num) {
-        console.log("login sucessfull");
-      } else {
-        console.log("failed");
-      }
-    });
-  } else {
-    console.log("jjj");
-  }
+  arr.forEach((num, pins) => {
+    // console.log(array);
+    if (pins === num) {
+      // return "success";
+      console.log("success");
+    } else {
+      console.log("failed");
+    }
+  });
+
+  // if (Array.isArray(arr)) {
+  //   arr.forEach((num) => {
+  //     console.log(num);
+  //   });
+  // } else {
+  //   console.log("jjj");
+  // }
 }
-atmValidation(["1111", "2222", "3333", "2345"], 3333);
+console.log(atmValidation([1111, 2222, 3333, 4444], 4444));
+
+// atm__cards = [122, 333, 44];
+// atm__pins = [123, 432, 145];
+
+// function atmValidation(arr, pins) {
+//   if (Array.isArray(arr)) {
+//     // arr.map((num) => {
+//     //   // console.log(num);
+//     //   if (pins === num) {
+//     //     console.log("login sucessfull");
+//     //   } else {
+//     //     console.log("failed");
+//     //   }
+//     // });
+//     arr.forEach(function () {
+//       console.log(number);
+//     });
+//   }
+// }
+// atmValidation([1111, 2222, 3333, 4444], 4444);
